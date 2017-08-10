76a04b287dc5ca7ab897043a8a52140dd91dbd3e
unkomorimori <m8443061293@dqnwara.com>
2017-08-10 20:39:52 +0900

    no comment.

---
+++ alt-button-click/sample.js
@@ -0,0 +1,9 @@
+var btnClicks = Rx.Observable.fromEvent($('#btn'), "click");
+ 
+btnClicks
+    .filter(function (value) {
+        return value.altKey;
+    })
+    .subscribe(function () {
+        console.log('altを押しましたね');
+    });
